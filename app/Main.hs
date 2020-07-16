dab0919f558f4f5406012acdd57170d4a1c4d477
Steven van den Broek <stevenvdb@live.nl>
2020-07-16 18:13:12 +0200

    Change to blinking line cursor on start of program

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
 import Lib (runBrickFlashcards)
-import System.Environment (getArgs)
 import BrickHelpers
 import Brick
 import Brick.Widgets.Center
+import System.Environment (getArgs)
+import System.Process (runCommand)
 
 main :: IO ()
-main = runBrickFlashcards
+main = do
+  _ <- runCommand "echo \\\\e[5 q"
+  runBrickFlashcards
 
 -- test :: Widget ()
 -- test = hCenter $ myStrWrap "Heey, this is a very long string of text, well maybe not that long... but pretty long in any case."
