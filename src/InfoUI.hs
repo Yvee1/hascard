705ac6167eb028b6ac79d1c33b8e6ef63cdd2377
Steven van den Broek <stevenvdb@live.nl>
2020-07-17 22:32:41 +0200

    Add info menu

---
+++ src/InfoUI.hs
@@ -0,0 +1,65 @@
+module InfoUI (runInfoUI) where
+
+import Brick
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+import Brick.Widgets.Center
+import Control.Monad (void)
+import qualified Graphics.Vty as V
+
+type Event = ()
+type Name = ()
+type State = ()
+
+app :: App State Event Name
+app = App 
+  { appDraw = (:[]) . const ui
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent = handleEvent
+  , appStartEvent = return
+  , appAttrMap = const theMap
+  }
+
+ui :: Widget Name
+ui =
+  joinBorders $
+  center $ 
+  withBorderStyle unicodeRounded $
+  border $
+  hLimitPercent 60 $
+  hLimit 40 $
+  hCenter (withAttr titleAttr (str "Info")) <=>
+  hBorder <=>
+  drawInfo
+
+handleEvent :: State -> BrickEvent Name Event -> EventM Name (Next State)
+handleEvent s (VtyEvent e) =
+    case e of
+      V.EvKey (V.KChar 'c') [V.MCtrl]  -> halt s
+      V.EvKey V.KEsc [] -> halt s
+      V.EvKey V.KEnter [] -> halt s
+      V.EvKey V.KDown [] -> vScrollBy (viewportScroll ()) 1 >> continue s
+      V.EvKey (V.KChar 'j') [] -> vScrollBy (viewportScroll ()) 1 >> continue s
+      V.EvKey V.KUp [] -> vScrollBy (viewportScroll ()) (-1) >> continue s
+      V.EvKey (V.KChar 'k') [] -> vScrollBy (viewportScroll ()) (-1) >> continue s
+      _ -> continue s
+
+titleAttr :: AttrName
+titleAttr = attrName "title"
+
+theMap :: AttrMap
+theMap = attrMap V.defAttr
+    [ (titleAttr, fg V.yellow) ]
+
+drawInfo :: Widget Name
+drawInfo = 
+  padLeftRight 1 $
+  vLimitPercent 60 $
+  viewport () Vertical (strWrap info)
+
+runInfoUI :: IO ()
+runInfoUI = void $ defaultMain app ()
+
+info :: String
+info = 
+  "Hascard is a command-line utility for reviewing notes. Cards are written in markdown-like syntax; for more info see cards/syntax.txt in the git repository. Use the --help flag for information on the command line options.\n\nControls:\n * Use arrows or the j and k keys for menu navigation\n * Enter confirms a selection\n * Use TAB or the arrow keys for navigating gaps in open questions\n * Use the c key for confirming multiple choice questions with more than 1 possible answer"
