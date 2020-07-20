8e7b6ef079cabf904e48cbe9a6e9e12ac07f382c
Sridhar Ratnakumar <srid@srid.ca>
2020-07-20 07:16:41 -0400

    Add Nix support
    This allows you to build the project with `nix-build`, as well as drop into a dev shell via `nix-shell`.

---
+++ default.nix
@@ -0,0 +1,11 @@
+let 
+  pkgs = import <nixpkgs> { };
+in 
+  pkgs.haskellPackages.developPackage {
+    root = ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [ cabal-install
+          ghcid
+        ]);
+  }
