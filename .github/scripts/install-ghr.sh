f46865555773879dc4effadda753bb1f7cc141b8
Steven van den Broek <stevenvdb@live.nl>
2021-07-17 12:07:44 +0200

    Try to fix ghr

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -13,8 +13,7 @@ else
   echo "Installing ghr"
   URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_${ARCH}_386.zip"
   curl -L ${URL} > ghr.zip
-  mkdir -p "$HOME/bin"
-  export PATH="$HOME/bin:$PATH"
-  unzip ghr.zip -d "$HOME/bin"
+  mkdir -p "$HOME/.local/bin"
+  unzip ghr.zip -d "$HOME/.local/bin/"
   rm ghr.zip
 fi
